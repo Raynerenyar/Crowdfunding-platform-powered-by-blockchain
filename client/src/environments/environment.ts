d98691cead677f4387d98e33f1cbe11b09485488
Raynerenyar <105347568+Raynerenyar@users.noreply.github.com>
2023-11-06 23:28:55 +0800

    changed backend url

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const constants = {
   // SERVER_URL: 'http://localhost:8080',
-  SERVER_URL: 'https://crowdfunding-server.up.railway.app',
+  SERVER_URL: 'https://crypto-crowdfunding.onrender.com',
 }
 
 export const environment = {
